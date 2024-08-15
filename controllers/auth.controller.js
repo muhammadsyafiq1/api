05f8f462f2260112e6b74a21fdfb2ce50c638f39
muhammadsyafiq1 <syafiqmuhammad655@gmail.com>
2024-08-15 21:58:28 +0700

    validasi

---
+++ controllers/auth.controller.js
@@ -0,0 +1,33 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        return res.status(400).json("Semua field harus diisi.");
+    }
+
+    try {
+        const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+        if (existingUser) {
+            return res.status(400).json("Username atau email sudah terdaftar.");
+        }
+
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword 
+        });
+
+        await newUser.save();
+        res.status(201).json("Pengguna berhasil dibuat");
+    } catch (error) {
+        console.error(error);
+        res.status(500).json("Terjadi kesalahan saat membuat pengguna");
+    }
+};
+
