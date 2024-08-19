cdcc58872a4306f26aecb8ccae364c1bdee11d34
muhammadsyafiq1 <syafiqmuhammad655@gmail.com>
2024-08-19 21:27:19 +0700

    sign in

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,33 +1,61 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken"
 
 export const signup = async (req, res, next) => {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    if (!username || !email || !password) {
-        return res.status(400).json({ success: false, message: "Semua field harus diisi." });
+  if (!username || !email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Semua field harus diisi." });
+  }
+
+  try {
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "Username atau email sudah terdaftar.",
+      });
     }
 
-    try {
-        const existingUser = await User.findOne({ $or: [{ username }, { email }] });
-        if (existingUser) {
-            return res.status(400).json({ success: false, message: "Username atau email sudah terdaftar." });
-        }
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
 
-        const salt = await bcryptjs.genSalt(10);
-        const hashedPassword = await bcryptjs.hash(password, salt);
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
 
-        const newUser = new User({
-            username,
-            email,
-            password: hashedPassword 
-        });
-
-        await newUser.save();
-        res.status(201).json({ success: true, message: "Pengguna berhasil dibuat" });
-    } catch (error) {
-        next(error)
-    }
+    await newUser.save();
+    res
+      .status(201)
+      .json({ success: true, message: "Pengguna berhasil dibuat" });
+  } catch (error) {
+    next(error);
+  }
 };
 
-
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const validUser = await User.findOne({ email });
+      if (!validUser) return next(errorHandler(404, "User tidak ditemukan"));
+  
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if (!validPassword) return next(errorHandler(401, "Password Salah"));
+  
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+  
+      res
+        .cookie('access_token', token, { httpOnly: true })
+        .status(200)
+        .json({ user: validUser, token });
+    } catch (error) {
+      next(error);
+    }
+  };
+  
